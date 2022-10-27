543c50a6036a6e51a576074e78547199217b22c7
flrvia <flavixbatista@gmail.com>
2022-10-27 19:59:33 -0300

    first commit

---
+++ blog-pessoal/src/paginas/home/Home.tsx
@@ -0,0 +1,17 @@
+import React from "react"; 
+import './Home.css';
+
+function Home(){
+    return(
+
+        <>
+            <h1 className="titulo" >Home</h1>
+            <p>Rick and Morty</p>
+            <img  src="https://imgur.com/PYCcL4D.png" alt="Imagem tela inicial" className="img"/>
+
+        </>
+
+    );
+}
+
+export default Home;
